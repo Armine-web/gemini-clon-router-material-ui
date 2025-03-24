8f54d5f864331f823c9fa570877e708d9450ca9e
Armine Aghajanyan <armine.aghajanyan123@gmail.com>
2025-03-24 20:29:29 +0400

    fixing

--- gemini-clon/vite.config.js
+++ gemini-clon/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/book-landing-page/gemini-clon-router-material-ui/" 
+  base: "/gemini-clon-router-material-ui/" 
 })
